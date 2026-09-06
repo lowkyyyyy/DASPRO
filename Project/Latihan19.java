7e6772b3e43a4d44f8ae13ff44e9e75ea7c332c9
lowkyyyyy <fauzanproklanesia@gmail.com>
2026-09-06 13:26:02 +0700

    Delete Project directory

--- Project/Latihan19.java
+++
@@ -1,7 +0,0 @@
-public class Latihan19 {
-    public static void main(String[] args) {
-        System.out.println("Halo, nama saya Mohammad Fauzan Proklanesia Donito");
-        System.out.println("Belajar  Java itu  mudah");
-        System.out.println("Semakin sering mencoba, semakin jago");
-    }
-}
--- Project/MyFirstJava19.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstJava19 {
-    public static void main(String[] args) {
-        System.out.println("Hallo, saya Fauzan");
-    }
-    
-}
